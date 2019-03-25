7a3c815e0280cbfa242cd299d90d8ad59e2c5f9d
dengxz <6882855@qq.com>
2019-03-25 18:59:25 +0800

    t

--- docs/deploy/update.sh
+++ docs/deploy/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf ./BOOT-INF/classes;
-unzip -q root-demo-0.0.1-SNAPSHOT.jar.original -d ./BOOT-INF/classes;
-zip -qd root-demo-0.0.1-SNAPSHOT.jar BOOT-INF/classes/*;
-zip -ur root-demo-0.0.1-SNAPSHOT.jar ./BOOT-INF/*;
+unzip -q repository-0.0.1-SNAPSHOT.jar.original -d ./BOOT-INF/classes;
+zip -qd repository-0.0.1-SNAPSHOT.jar BOOT-INF/classes/*;
+zip -ur repository-0.0.1-SNAPSHOT.jar ./BOOT-INF/*;
