6cb18bcbd93bf9093f4e7271c2c68a03d240072e
dengxz <6882855@qq.com>
2019-04-09 18:04:38 +0800

    t

--- common/src/main/resources/static/mobile/m-common-0.1.0.js
+++ common/src/main/resources/static/mobile/m-common-0.1.0.js
@@ -99,4 +99,14 @@ function hideLoading() {
 }
 
 
+function htmlEscape(text){ 
+	  return text.replace(/[<>"&]/g, function(match, pos, originalText){
+	    switch(match){
+	    case "<": return "&lt;"; 
+	    case ">":return "&gt;";
+	    case "&":return "&amp;"; 
+	    case "\"":return "&quot;"; 
+	  } 
+	}); 
+}
 
